e1989cc31a77e0e7dc22688101a6d85d15cccfff
hari2k7 <hari082007@gmail.com>
2026-05-09 22:57:38 +0530

    Add PORT env

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ mongoose.connect(process.env.MONGO_URI)
 app.use('/notes',noteRoutes);
 app.use('/api/auth', authRoutes)
 
-const PORT = 5000
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, ()=> {
     console.log('Server running on port 5000')
