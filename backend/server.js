5b309f095007d55eca90c54686578c2557d0e1af
hari2k7 <hari082007@gmail.com>
2026-05-09 23:13:23 +0530

    Add production frontend url to cors

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,11 @@ dotenv.config()
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+  origin: 'https://quick-notes-nine-jet.vercel.app',
+  credentials: true,
+}));
+
 app.use(express.json())
 
 mongoose.connect(process.env.MONGO_URI)
